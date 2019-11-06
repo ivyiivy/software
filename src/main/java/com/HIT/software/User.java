581f903821202eadefd6bc27e706ec0f2b4a260a
dakota <43801014+ivyiivy@users.noreply.github.com>
2019-11-06 14:30:51 +0800

    Initial commit

---
+++ src/main/java/com/HIT/software/User.java
@@ -0,0 +1,58 @@
+package com.HIT.software;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String isAdmin;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void setIsAdmin(String  isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
---
+++ src/main/java/com/HIT/software/controller.java
@@ -0,0 +1,71 @@
+package com.HIT.software;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+
+@Controller
+public class controller {
+
+    private UserRepository userRepository;
+    private static final String isAdmin = "0";
+
+    @Autowired
+    public controller(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/index")
+    public String signIn() {
+        return "index";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signup";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/signup")
+    public String signUP(User user,Map<String,Object> map) {
+        User user1=userRepository.findByEmail(user.getEmail());
+        if (user1!=null){
+            map.put("msg","此邮箱已注册");
+            return "signup";
+        }
+        user.setIsAdmin(isAdmin);
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
+
+    @PostMapping("/index")
+    public String signIn(User user, Map<String,Object> map) {
+
+        User user1 = userRepository.findByEmail(user.getEmail());
+        if (user1 == null) {
+            map.put("msg","没有此用户");
+            return "index";
+        }
+        if (!user1.getPassword().equals(user.getPassword())) {
+            map.put("msg","密码错误");
+            return "index";
+        }
+
+        return "redirect:/hello";
+
+
+    }
+
+
+}
